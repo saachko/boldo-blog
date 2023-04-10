c1b1b96d6aef75dc0d1627f7898df19724c797ee
Anastasiya Sachko <baron-nastya2009@yandex.ru>
2023-04-11 02:56:20 +0300

    fix: remove unnecessary files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
-import 'styles/globals.css';
 import type { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
+import 'styles/reset.scss';
+import 'styles/globals.scss';
+
+function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
+
+export default App;
