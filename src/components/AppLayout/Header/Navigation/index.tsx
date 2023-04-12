765a5f2e0ec1273bfedb4c0ec4ed2377ee830f3d
Anastasiya Sachko <baron-nastya2009@yandex.ru>
2023-04-12 16:18:25 +0300

    feat: add burger menu

--- src/components/AppLayout/Header/Navigation/index.tsx
+++ src/components/AppLayout/Header/Navigation/index.tsx
@@ -3,6 +3,8 @@ import cn from 'classnames';
 
 import openSans from 'utils/fonts/openSans';
 
+import useNavigation from 'hooks/useNavigation';
+
 import Button from 'components/Button';
 
 import { NavigationLinkItem } from 'ts/interfaces';
@@ -14,9 +16,33 @@ interface NavigationProps {
 }
 
 function Navigation({ navigationList }: NavigationProps) {
+  const { isMenuOpen, closeMenu, toggleMenu } = useNavigation();
+
+  const burgerClassName = cn(styles.burger, {
+    [styles['burger--active']]: isMenuOpen,
+  });
+
+  const menuClassName = cn(styles.menu, {
+    [styles['menu--active']]: isMenuOpen,
+  });
+
+  const overlayClassName = cn(styles.overlay, {
+    [styles['overlay--active']]: isMenuOpen,
+  });
+
   return (
     <nav className={styles.navigation}>
-      <ul className={styles.list}>
+      <button type="button" className={burgerClassName} onClick={toggleMenu}>
+        <span className={styles.bar} />
+        <span className={styles.bar} />
+        <span className={styles.bar} />
+      </button>
+      <div
+        role="presentation"
+        className={overlayClassName}
+        onClick={closeMenu}
+      />
+      <ul className={menuClassName}>
         {navigationList.map(({ id, href, text }) => (
           <li key={id} className={styles.item}>
             <Link href={href} className={cn(styles.link, openSans.className)}>
